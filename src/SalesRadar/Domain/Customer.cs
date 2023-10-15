e507f1c084131a5fe7a7af14643ee9f78ced93bc
Hakim Ghods <ghods.hakim@hotmail.com>
2023-10-15 13:01:54 +0330

    Add project files.

---
+++ src/SalesRadar/Domain/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesRadar.Domain;
+public class Customer
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public required string PhoneNumber { get; set; }
+    public required string Email { get; set; }
+    public required string BankAccountNumber { get; set; }
+}
