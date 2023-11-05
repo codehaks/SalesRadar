1be74fda2ad2d521c00daa3d1645832028a44a2a
Hakim Ghods <ghods.hakim@hotmail.com>
2023-11-05 11:36:57 +0330

    clean

--- test/SalesRadar.Tests/UnitTests/DateOfBirthTests.cs
+++ test/SalesRadar.Tests/UnitTests/DateOfBirthTests.cs
@@ -7,39 +7,7 @@ namespace SalesRadar.Tests.UnitTests;
 
 public class DateOfBirthTests
 {
-    [Fact]
-    public void DateOfBirth_Value_ShouldBeSetCorrectly()
-    {
-        // Arrange
-        DateTime expectedDate = new DateTime(2000, 1, 1);
-
-        // Act
-        DateOfBirth dateOfBirth = new DateOfBirth(expectedDate);
-
-        // Assert
-        Assert.Equal(expectedDate, dateOfBirth.Value);
-    }
-
-    [Fact]
-    public void DateOfBirth_WithValidDate_ShouldNotThrowException()
-    {
-        // Arrange
-        DateTime validDate = DateTime.UtcNow.Date.AddYears(-30);
-
-        // Act & Assert
-        var ex=Record.Exception(() => new DateOfBirth(validDate));
-        Assert.Null(ex);
-    }
-
-    [Fact]
-    public void DateOfBirth_WithFutureDate_ShouldThrowException()
-    {
-        // Arrange
-        DateTime futureDate = DateTime.UtcNow.Date.AddYears(1);
-
-        // Act & Assert
-        Assert.Throws<ArgumentOutOfRangeException>(() => new DateOfBirth(futureDate));
-    }
+   
 
     [Fact]
     public void DateOfBirth_WithCurrentDate_ShouldNotThrowException()
--- update-coverage-report.bat
+++
@@ -1,4 +0,0 @@
-echo off
-cd ..\..\
-coverlet .\test\SalesRadar.Tests\bin\Debug\net8.0\SalesRadar.Tests.dll --target "dotnet" --targetargs "test --no-build" -f OpenCover
-reportgenerator -reports:"coverage.opencover.xml" -targetdir:"coverage-results" -reportTypes:HtmlInline
